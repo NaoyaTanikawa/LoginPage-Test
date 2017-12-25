d6398d8e8d709cba431a84c34214819041def863
NaoyaTanikawa <34472406+NaoyaTanikawa@users.noreply.github.com>
2017-12-25 10:38:50 +0900

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var mypage = require('./routes/mypage');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
